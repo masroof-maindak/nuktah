e3bc4229f4f072e20c7e71efd8b1982278dc2d0b
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-02-17 21:48:13 +0500

    [WIP] feat: strtok

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+use nuktah::compile_src;
+
 fn main() -> std::io::Result<()> {
     let mut f = File::open("src.nkt")?;
     let mut src_code = String::new();
 
     f.read_to_string(&mut src_code)?;
-    print!("{src_code}");
-    Ok(())
+
+    match compile_src(&mut src_code) {
+        Ok(()) => {
+            println!("Built in {} seconds.", 10);
+            return Ok(());
+        }
+
+        Err(e) => {
+            eprintln!("{e}");
+            std::process::exit(1)
+        }
+    };
 }
