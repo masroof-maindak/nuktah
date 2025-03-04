50f158f42315a3bb18e1a02a896389b6003006f0
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-03-04 13:18:10 +0500

    chore: adjust project structure & includes for parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
 pub mod lexer;
+pub mod parser;
 
-pub enum CompilerError {
-    Nil,
-    LexerError,
-}
+// pub enum CompilerError {
+//     Nil,
+//     LexerError,
+// }
 
 pub fn compile_src(src_code: &mut String) -> Result<(), &'static str> {
+    // CHECK: should I pass a symbol table to this too?
     let tokens = lexer::lexer::tokenize_src_code(src_code)?;
+
     println!("{:?}", tokens);
+
+    let _ = parser::parser::parse_token_list(tokens);
 
     Ok(())
 }
---
+++ src/parser/parser.rs
@@ -0,0 +1,3 @@
+use crate::lexer::token::Token;
+
+pub fn parse_token_list(_tokens: Vec<Token>) {}
