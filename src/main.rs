03ba49f17af93db7fb4620944e6f1657542fb21c
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-03-06 22:52:04 +0500

    fix: time compilation instead of file-loading

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ use nuktah::compile_src;
 fn main() -> std::io::Result<()> {
     let mut f = File::open("src.nkt")?;
     let mut src_code = String::new();
+    f.read_to_string(&mut src_code)?;
 
     let start = Instant::now();
-    f.read_to_string(&mut src_code)?;
+    let res = compile_src(&mut src_code);
     let duration = start.elapsed();
 
-    match compile_src(&mut src_code) {
+    match res {
         Ok(()) => {
             println!("Built in {} seconds.", duration.as_secs_f64());
             return Ok(());
