30f11504794d9227d24a7d0a88572b4cb6c32b45
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-06-09 18:18:57 +0500

    fix(semantics): update errors

--- src/semantics/errors.rs
+++ src/semantics/errors.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub enum ScopeError {
-    UndefinedIdentifierUsed,
+    UndeclaredVariableCalled,
+    UndefinedFunctionCalled,
     VariableRedefinition,
     FunctionPrototypeRedefinition,
 }
